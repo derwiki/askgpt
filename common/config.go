5357342ba668373a1212ffed50e59b139d7defb8
Adam Derewecki <a@der.wiki>
2023-06-06 20:39:36 -0700

    Add logging to config

--- common/config.go
+++ common/config.go
@@ -22,9 +22,21 @@ type Config struct {
 }
 
 func LoadConfig() (Config, error) {
+	// parse info first so other info logs in this file are outputted
+	var useInfo bool
+	flag.BoolVar(&useInfo, "info", false, "If set, show info and above logs")
+	flag.Parse()
+	log.Info().Msg(fmt.Sprintf("config useInfo: %b", useInfo))
+
+	if useInfo {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
+	log.Info().Msg(fmt.Sprintf("config LoadConfig() enter"))
 	config := Config{}
 
 	config.PromptPrefix = os.Getenv("PROMPT_PREFIX")
+	log.Info().Msg(fmt.Sprintf("config.PromptPrefix: %s", config.PromptPrefix))
 	num, err := strconv.Atoi(os.Getenv("HISTORY_LINE_COUNT"))
 	if err != nil {
 		config.HistoryLineCount = 10 // default
@@ -33,9 +45,12 @@ func LoadConfig() (Config, error) {
 	}
 
 	config.OpenAIApiKey = os.Getenv("OPENAI_API_KEY")
+	log.Info().Msg(fmt.Sprintf("config.OpenAIApiKey length: %s", len(config.OpenAIApiKey)))
 	config.BardApiKey = os.Getenv("BARDAI_API_KEY")
+	log.Info().Msg(fmt.Sprintf("config.BardApiKey length: %s", len(config.BardApiKey)))
 
 	// read LLM models as an array
+	log.Info().Msg(fmt.Sprintf("config LLM_MODELS: %s", os.Getenv("LLM_MODELS")))
 	models := strings.Split(os.Getenv("LLM_MODELS"), ",")
 	if models[0] != "" {
 		config.LLMModels = models
@@ -44,6 +59,7 @@ func LoadConfig() (Config, error) {
 	}
 
 	maxTokensStr := os.Getenv("MAX_TOKENS")
+	log.Info().Msg(fmt.Sprintf("config maxTokenStr: %s", maxTokensStr))
 	if maxTokensStr == "" {
 		config.MaxTokens = 200
 	} else {
@@ -57,26 +73,18 @@ func LoadConfig() (Config, error) {
 	var skipHistory bool
 	var useGpt4 bool
 	var useBard bool
-	var useInfo bool
 	flag.BoolVar(&skipHistory, "skip-history", false, "If set, history will not be written to or read from.")
 	flag.BoolVar(&useGpt4, "gpt4", false, "If set, shortcut to LLM_MODELS=gpt-4")
 	flag.BoolVar(&useBard, "bard", false, "If set, shortcut to LLM_MODELS=bard")
-	flag.BoolVar(&useInfo, "info", false, "If set, show info and above logs")
 	flag.Parse()
-	log.Info().Msg(fmt.Sprintf("skipHistory: %b", skipHistory))
-	log.Info().Msg(fmt.Sprintf("useGpt4: %b", useGpt4))
-	log.Info().Msg(fmt.Sprintf("useBard: %b", useBard))
-	log.Info().Msg(fmt.Sprintf("useInfo: %b", useInfo))
+	log.Info().Msg(fmt.Sprintf("config skipHistory: %b", skipHistory))
+	log.Info().Msg(fmt.Sprintf("config useGpt4: %b", useGpt4))
+	log.Info().Msg(fmt.Sprintf("config useBard: %b", useBard))
 	config.SkipHistory = skipHistory
 	if useGpt4 {
 		config.LLMModels = []string{openai.GPT4}
 	} else if useBard {
 		config.LLMModels = []string{"bard"}
 	}
-
-	if useInfo {
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	}
-
 	return config, nil
 }
