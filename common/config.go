de8dba47f38ece59ddaacfefb04b1afe77e971b8
Adam Derewecki <a@der.wiki>
2023-12-28 18:58:28 -0800

    aider: Add `claude` CLI option, store it in useClaude, and wire it up after checking useGpt4 and useBard at the end.
    # Aider chat conversation:
    
    USER: Add a `bard` CLI option like `gpt4` and `bard`, store it in useClaude, and add an else if to wire it up after checking useGpt4 and useBard at the end.
    
    ASSISTANT: ```diff
    --- common/config.go
    +++ common/config.go
    @@ ... @@
     	var useInfo bool
     	var skipHistory bool
     	var useGpt4 bool
    +	var useClaude bool
     	var useBard bool
     	flag.BoolVar(&useInfo, "info", false, "If set, show info and above logs")
     	flag.BoolVar(&skipHistory, "skip-history", false, "If set, history will not be written to or read from.")
    @@ ... @@
     	flag.BoolVar(&useBard, "bard", false, "If set, shortcut to LLM_MODELS=bard")
    +	flag.BoolVar(&useClaude, "claude", false, "If set, shortcut to LLM_MODELS=claude-2.1")
     	flag.Parse()
     	log.Info().Msg(fmt.Sprintf("config useInfo: %b", useInfo))
     	log.Info().Msg(fmt.Sprintf("config skipHistory: %b", skipHistory))
    @@ ... @@
     	log.Info().Msg(fmt.Sprintf("config useBard: %b", useBard))
    +	log.Info().Msg(fmt.Sprintf("config useClaude: %b", useClaude))
    
     	if useInfo {
     		zerolog.SetGlobalLevel(zerolog.InfoLevel)
    @@ ... @@
     	config.SkipHistory = skipHistory
     	if useGpt4 {
     		config.LLMModels = []string{openai.GPT4TurboPreview}
     	} else if useBard {
     		config.LLMModels = []string{"bard"}
    +	} else if useClaude {
    +		config.LLMModels = []string{"claude-2.1"}
     	}
     	return config, nil
     }
    ```

--- common/config.go
+++ common/config.go
@@ -28,16 +28,19 @@ func LoadConfig() (Config, error) {
 	var useInfo bool
 	var skipHistory bool
 	var useGpt4 bool
+	var useClaude bool
 	var useBard bool
 	flag.BoolVar(&useInfo, "info", false, "If set, show info and above logs")
 	flag.BoolVar(&skipHistory, "skip-history", false, "If set, history will not be written to or read from.")
 	flag.BoolVar(&useGpt4, "gpt4", false, "If set, shortcut to LLM_MODELS=gpt-4-1106-preview")
 	flag.BoolVar(&useBard, "bard", false, "If set, shortcut to LLM_MODELS=bard")
+	flag.BoolVar(&useClaude, "claude", false, "If set, shortcut to LLM_MODELS=claude-2.1")
 	flag.Parse()
 	log.Info().Msg(fmt.Sprintf("config useInfo: %b", useInfo))
 	log.Info().Msg(fmt.Sprintf("config skipHistory: %b", skipHistory))
 	log.Info().Msg(fmt.Sprintf("config useGpt4: %b", useGpt4))
 	log.Info().Msg(fmt.Sprintf("config useBard: %b", useBard))
+	log.Info().Msg(fmt.Sprintf("config useClaude: %b", useClaude))
 
 	if useInfo {
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
@@ -88,6 +91,8 @@ func LoadConfig() (Config, error) {
 		config.LLMModels = []string{openai.GPT4TurboPreview}
 	} else if useBard {
 		config.LLMModels = []string{"bard"}
+	} else if useClaude {
+		config.LLMModels = []string{"claude-2.1"}
 	}
 	return config, nil
 }
